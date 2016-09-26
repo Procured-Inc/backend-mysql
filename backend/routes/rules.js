2c56b3fc348b54f12b8b4707e470e1fd0400d2eb
SAP0312 <shubhamapatidar143@gmail.com>
2016-09-26 17:07:56 +0530

    popup removed

--- backend/routes/rules.js
+++ backend/routes/rules.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET rules page. */
 router.get('/', function(req, res, next) {
     req.session.testid=req.query.test_id;
+    console.log(req.session.testid,'testid')
     res.render('rules',{ testid: req.query.test_id,s_id:req.session.sid });
 });
 
